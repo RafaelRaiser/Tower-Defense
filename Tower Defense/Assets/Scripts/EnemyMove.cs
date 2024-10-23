9c815f20b92edfe1eb2fb0038687be29acde61d6
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-10-23 13:38:43 -0300

    Uodate

--- Tower Defense/Assets/Scripts/EnemyMove.cs
+++ Tower Defense/Assets/Scripts/EnemyMove.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Rigidbody2D rB;
+    [SerializeField] private float moveSpeed = 2f;
 }
