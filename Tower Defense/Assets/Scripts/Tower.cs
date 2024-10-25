48c6375df61349d11dc4cd82e357a754773f7ecf
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-10-25 14:00:57 -0300

    Update Tower.cs

--- Tower Defense/Assets/Scripts/Tower.cs
+++ Tower Defense/Assets/Scripts/Tower.cs
@@ -21,5 +21,28 @@ public class Tower : MonoBehaviour
             }
         }
     }
+    private Enemy EncontrarInimigo()
+    {
+        Collider2D[] inimigos = Physics2D.OverlapCircleAll(transform.position, alcance);
+        Enemy alvo = null;
+        foreach (var col in inimigos)
+        {
+            if (col.CompareTag("Enemy"))
+            {
+                alvo = col.GetComponent<Enemy>();
+                break;
+            }
+        }
+        return alvo;
+    }
+    private void Disparar(Enemy inimigo)
+    {
+        GameObject projetilGO = (GameObject)Instantiate(projetilPrefab, transform.position, transform.rotation);
+        Projectile projetil = projetilGO.GetComponent<Projectile>();
+        if (projetil != null)
+        {
+            projetil.DefinirAlvo(inimigo.transform);
+        }
+    }
 
 }
